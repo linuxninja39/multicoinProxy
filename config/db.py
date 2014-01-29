e84c2365bfcd75ed70b7dc52f79010795bcd66f5
ivanm11 <ivan@core-tech.ru>
2014-01-29 08:13:00 -0700

    Basic user authorization using SQLAlchemy.

--- config/db.py
+++ config/db.py
@@ -1,10 +1,45 @@
-from sqlalchemy import create_engine
+from sqlalchemy import schema, create_engine
 from sqlalchemy.orm import sessionmaker
-
 import pprint
+import argparse
+def load_src(name, fpath):
+    import os, imp
+    return imp.load_source(name, os.path.join(os.path.dirname(__file__), fpath))
 
-Session = sessionmaker()
+load_src("models", "../model/models.py")
+import models
 
-dbEngine = create_engine('mysql+mysqldb://root@localhost/MultiPool', echo=True)
+def db_parse_args():
+    db_parser = argparse.ArgumentParser(description='This proxy allows you to run getwork-based miners against Stratum mining pool.')
+    db_parser.add_argument('-d', '--drop', dest='drop', action='store_true', help='Drop tables')
+    db_parser.add_argument('-u', '--update', dest='update', action='store_true', help='Update tables')
+    db_parser.add_argument('-a', '--add', dest='add', type=str, help='Add row to table')
+    return db_parser.parse_args()
 
-Session.configure(bind=dbEngine)
+dbEngine = create_engine('mysql+mysqldb://root:jfdojfoed8@localhost/NewMultiPool', echo=True)
+
+if __name__ == '__main__':
+    db_args = db_parse_args()
+    metadata = models.Base.metadata
+    metadata.bind = dbEngine
+    Session = sessionmaker(bind=dbEngine)
+    session = Session()
+    if db_args.drop:
+        print 'DROPPING TABLES...'
+        metadata.drop_all()
+    if db_args.update:
+        print 'CREATING TABLES...'
+        metadata.create_all()
+    if db_args.add != None or db_args.add != 'user':
+        user = models.User()
+        for property, value in vars(models.User).iteritems():
+            if property[:1] != "_" and property != 'coins' and property != 'id':
+                # print ("Enter %s:" , property)
+                text = "Enter %s:" % property
+                value = input(text)
+                print(value)
+                setattr(user, property, value)
+                # user.property = value
+        session.add(user)
+        session.commit()
+
--- mining_libs/worker_registry.py
+++ mining_libs/worker_registry.py
@@ -1,60 +1,67 @@
 import time
 
 import stratum.logger
+from config.db import dbEngine
 from mining_libs.user_mapper import UserMapper
+from model.models import User
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import and_
 
 log = stratum.logger.get_logger('proxy')
 
 class WorkerRegistry(object):
-	userMapper = UserMapper()
+    userMapper = UserMapper()
+    Session = sessionmaker(bind=dbEngine)
+    session = Session()
 
-	def __init__(self, f):
-		self.f = f # Factory of Stratum client
-		self.clear_authorizations()
+    def __init__(self, f):
+        self.f = f # Factory of Stratum client
+        self.clear_authorizations()
 
-	def clear_authorizations(self):
-		self.authorized = []
-		self.unauthorized = []
-		self.last_failure = 0
+    def clear_authorizations(self):
+        self.authorized = []
+        self.unauthorized = []
+        self.last_failure = 0
 
-	def _on_authorized(self, result, worker_name):
-		if result == True:
-			self.authorized.append(worker_name)
-		else:
-			self.unauthorized.append(worker_name)
-		return result
+    def _on_authorized(self, result, worker_name):
+        if result == True:
+            self.authorized.append(worker_name)
+        else:
+            self.unauthorized.append(worker_name)
+        return result
 
-	def _on_failure(self, failure, worker_name):
-		log.exception("Cannot authorize worker '%s'" % worker_name)
-		self.last_failure = time.time()
+    def _on_failure(self, failure, worker_name):
+        log.exception("Cannot authorize worker '%s'" % worker_name)
+        self.last_failure = time.time()
 
-	def authorize(self, worker_name, password):
-		if worker_name in self.authorized:
-			return True
+    def authorize(self, worker_name, password):
+        if worker_name in self.authorized:
+            return True
 
-		if worker_name in self.unauthorized and time.time() - self.last_failure < 60:
-			# Prevent flooding of mining.authorize() requests 
-			log.warning("Authentication of worker '%s' with password '%s' failed, next attempt in few seconds..." % \
-					(worker_name, password))
-			return False
+        if worker_name in self.unauthorized and time.time() - self.last_failure < 60:
+            # Prevent flooding of mining.authorize() requests
+            log.warning("Authentication of worker '%s' with password '%s' failed, next attempt in few seconds..." % \
+                    (worker_name, password))
+            return False
 
-		remoteUser = self.userMapper.getUser(worker_name, password)
-		if not remoteUser:
-			log.warning("Couldn't map user '%s' with password '%s'" % (worker_name, password))
-			self._on_failure('', worker_name)
-			return False
+        remote_user = self.session.query(User).filter_by(username = worker_name, password = password).one()
+        # remoteUser = self.userMapper.getUser(worker_name, password)
+        if not remote_user:
+            log.warning("Couldn't map user '%s' with password '%s'" % (worker_name, password))
+            self._on_failure('', worker_name)
+            return False
 
-		log.info("Local user/pass '%s:%s'. Remote user/pass '%s:%s'" % \
-				(worker_name, password, remoteUser['remoteUser'], remoteUser['remotePassword'])
-				)
+        log.info("Local user/pass '%s:%s'. Remote user/pass '%s:%s'" % \
+                (worker_name, password, remote_user.remote_username, remote_user.remote_password)
+                )
 
-		d = self.f.rpc('mining.authorize', [remoteUser['remoteUser'], remoteUser['remotePassword']])
-		d.addCallback(self._on_authorized, worker_name)
-		d.addErrback(self._on_failure, worker_name)
-		return d
+        d = self.f.rpc('mining.authorize', [remote_user.remote_username, remote_user.remote_password])
+        d.addCallback(self._on_authorized, worker_name)
+        d.addErrback(self._on_failure, worker_name)
+        return d
 
-	def is_authorized(self, worker_name):
-		return (worker_name in self.authorized)
+    def is_authorized(self, worker_name):
+        return (worker_name in self.authorized)
 
-	def is_unauthorized(self, worker_name):
-		return (worker_name in self.unauthorized)  
+    def is_unauthorized(self, worker_name):
+        return (worker_name in self.unauthorized)
